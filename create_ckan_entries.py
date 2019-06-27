4ecffc20ef14147f7df19391230a063e6a09e07d
Toni Alatalo <toni@playsign.net>
2019-06-27 14:37:09 +0300

    ckan api call to create a new dataset

---
+++ create_ckan_entries.py
@@ -0,0 +1,30 @@
+#pip install ckanapi
+import json
+from ckanapi import RemoteCKAN
+import pprint
+from pprint import pprint
+
+# Put the details of the dataset we're going to create into a dict.
+dataset_dict = {
+    'name': 'autosync_test_name',
+    'notes': 'autosync test notes',
+    'owner_org': 'cityiot'
+}
+
+c = RemoteCKAN('http://pan0109.panoulu.net:5000/', apikey='555f89e9-2610-41c4-ae38-9148d1b4bba0')
+#s = c.action.package_list()
+#pprint(s)
+
+orgs = c.action.organization_list()
+pprint(orgs)
+
+#https://docs.ckan.org/en/latest/api/index.html#ckan.logic.action.create.package_create
+c.action.package_create(**dataset_dict)
+
+# Use the json module to load CKAN's response into a dictionary.
+#response_dict = json.loads(response.read())
+#assert response_dict['success'] is True
+
+# package_create returns the created package as its result.
+#created_package = response_dict['result']
+#pprint(created_package)
