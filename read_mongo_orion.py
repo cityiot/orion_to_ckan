80fd2963aec79cc8a09e94c19fb4a2661823d263
Toni Alatalo <toni@playsign.net>
2019-06-27 13:31:45 +0300

    data registry / orion discoverability: Python code read Orion db names and list their services paths

--- read_mongo_orion.py
+++ read_mongo_orion.py
@@ -1,5 +1,7 @@
 #pip3 install pymongo
 
+import pprint
+from pprint import pprint
 import pymongo
 from pymongo import MongoClient
 
@@ -7,18 +9,33 @@ c = MongoClient()
 #MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True)
 #c.admin.command('ismaster')
 
-c.list_database_names()
+dbnames = c.list_database_names()
 #['admin', 'cep', 'iotagentul', 'local', 'orion', 'orion-testi', 'sth_testi']
+#pprint(dbnames)
 
-d = c['orion']
-d.list_collection_names()
-#['entities']
+#starting with orion-
+orion_dbs = [n.split('orion-')[1] for n in dbnames 
+             if n.startswith('orion-')]
+#pprint(orion_dbs)
 
+#check if matching STH DB exists to filter out orion-only tests (not real datasets i presume)
+filtered = [n for n in orion_dbs
+            if 'sth_'+n in dbnames]
+pprint(filtered)
+
+#mongo aggregate query to find FIWARE Service Paths in a Orion DB
 pipeline = [
     {"$group": {"_id": "$_id.servicePath"}}
-    ]
+]
 
-a = d.entities.aggregate(pipeline)
-#<pymongo.command_cursor.CommandCursor object at 0x7fe502666470>
-import pprint
-pprint.pprint(list(a))
+for n in filtered:
+    d = c['orion-'+n]
+    d.list_collection_names()
+    #['entities']
+
+    a = d.entities.aggregate(pipeline)
+    #<pymongo.command_cursor.CommandCursor object at 0x7fe502666470>
+
+    print("<--- Service Paths in DB {} ---".format(n)) #servers have py 3.5, f'' new format strings were only introduced in 3.6
+    pprint(list(a))
+    print("--->")
