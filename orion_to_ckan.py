f04af4aa4d33327be5d1182db88f03292f1b1923
Toni Alatalo <toni@playsign.net>
2019-06-28 10:53:46 +0300

    option to either read live mongo data from db, or use a previously read info from json dump. mostly to allow dev and test runs without mongo, maybe useful otherwise too

--- orion_to_ckan.py
+++ orion_to_ckan.py
@@ -8,6 +8,10 @@ creates CKAN Data Set entries accordingly for publishing the IoT metadata on the
 import read_mongo_orion
 import create_ckan_entries
 
+import json #here only for working with snapshot data, instead of reading live mongo db
+
+READ_MONGO = True #else use snapshot from json stored from a previous Mongo DB read
+
 def make_dataset_dict(name, notes):
     dataset_dict = {
         'name': name,
@@ -16,7 +20,19 @@ def make_dataset_dict(name, notes):
     }
     return dataset_dict
 
-db2servicepaths = read_mongo_orion.get_infos()
+db2servicepaths = None
+
+if READ_MONGO:
+    db2servicepaths = read_mongo_orion.get_infos()
+
+    #save snapshot of db read
+    with open('db2servicepaths.json', 'w') as outfile:  
+        json.dump(db2servicepaths, outfile)
+
+else: #use snapshot of db read
+    with open('db2servicepaths.json', 'r') as json_file:  
+        db2servicepaths = json.load(json_file)
+
 for dbname, servicepaths in db2servicepaths.items():
     dsd = make_dataset_dict(dbname, "Service Paths: {}".format(servicepaths))
     create_ckan_entries.create_dataset(dsd)
