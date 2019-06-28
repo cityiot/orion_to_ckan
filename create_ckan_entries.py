e2ceca4a1ec80f098fcd185df848a49c5bab7d3b
Toni Alatalo <toni@playsign.net>
2019-06-28 15:37:57 +0300

    dataregistry: cache ckan info about existing data. patch updates if entry already exists. allows manual addition of extra data fields in CKAN

--- create_ckan_entries.py
+++ create_ckan_entries.py
@@ -6,19 +6,47 @@ from pprint import pprint
 
 c = RemoteCKAN('http://pan0109.panoulu.net:5000/', apikey='555f89e9-2610-41c4-ae38-9148d1b4bba0')
 
-def get_or_create_dataset(dataset_dict):    
-    datasets = c.action.package_list() #could cache but just re-get for simplicity and safety now, the load does not matter here
-    pprint(datasets)
+def get_packageinfos():
+    package_list_with_resources = c.action.current_package_list_with_resources()
+    #pprint(package_list_with_resources)
+    
+    packageinfos = {}
+    for packageinfo in package_list_with_resources:
+        #print(packageinfo['name'])
+        name = packageinfo['name']
+        packageinfos[name] = packageinfo
+
+    return packageinfos
+
+#global cache populated at start, to support get-or-create both for datasets (packages) and resources
+packageinfos = get_packageinfos()
+
+def get_or_create_dataset(dataset_dict):
+    #datasets = c.action.package_list() #could cache but just re-get for simplicity and safety now, the load does not matter here
+    #pprint(datasets)
     name = dataset_dict['name']
+    print("[get_or_create_dataset] - {}".format(name))
     package = None
-    if name in datasets:
+    if name in packageinfos: #datasets:        
         package = c.action.package_show(id=name)
-    else:    
+    else:
+        #print("{} not in {}".format(name, packageinfos))
+        #pprint(packageinfos)
+        #print(packageinfos[name])
         #https://docs.ckan.org/en/latest/api/index.html#ckan.logic.action.create.package_create
         package = c.action.package_create(**dataset_dict)
         #pprint(package)
     return package
-    
+
+def find_existing_resource(package_name, service_path):
+    cached_package = packageinfos.get(package_name, False)
+    if cached_package: #was on server at the start of this whole run (module import)
+        existing_resources = cached_package['resources']
+        for r in existing_resources: #is a list where our 'unique key' is just a value..
+            if r['service_path'] == service_path:
+                return r
+    return None
+
 def create_resource(package, url, tenant, service_path):
     #https://docs.ckan.org/en/latest/api/index.html#ckan.logic.action.create.resource_create
     
@@ -33,16 +61,25 @@ def create_resource(package, url, tenant, service_path):
         'auth_type': 'none'
         #'resource_type': 'fiware-ngsi' #is this same as 'format' in gui?
     }
-    resource = c.action.resource_create(**resource_dict)
+
+    package_name = package['name']
+    resource = find_existing_resource(package_name, service_path)
+    if resource is not None: #exists, let's patch
+        print("Patching existing Resource:")
+        pprint(resource)
+        resource_dict['id'] = resource['id']
+        resource = c.action.resource_patch(**resource_dict)        
+    else:
+        resource = c.action.resource_create(**resource_dict)
+    
+        c.action.resource_view_create(
+            resource_id = resource['id'],
+            title = 'NGSI View',
+            description = 'This is a NGSI View',
+            view_type = 'ngsi_view'
+            )
+
     #pprint(resource)
-
-    c.action.resource_view_create(
-        resource_id = resource['id'],
-        title = 'NGSI View',
-        description = 'This is a NGSI View',
-        view_type = 'ngsi_view'
-        )
-
     return resource
 
 def test_update():
@@ -54,8 +91,10 @@ def test_update():
     }
 
     package = get_or_create_dataset(dataset_dict)
+    pprint(package)
     #package = {'id': '5077a989-3c66-4aa0-8031-ae832c46866e'} #'autosync_test_name13'
-    create_resource(package, "http://example.com", "tenant", "/servicepath")
+    resource = create_resource(package, "http://example.com/2", "tenant", "/servicepath")
+    pprint(resource)
 
 if __name__ == '__main__':
     test_update()
